26672022e9a8e3e8d0311cbb9bc55deb6de2b3a7
garudakarate-jepara <63992738+garudakarate-jepara@users.noreply.github.com>
2020-04-20 16:09:53 +0700

    Third Commit - Added Profil Instruktur

---
+++ js/profilinstruktur.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+  $(".light-heading-profilinstruktur h2")
+  .animate({
+    opacity: '1',
+    top: '0'
+  }, 1000);
+
+  $(".instruktur-1").delay(500)
+  .animate({
+    opacity: '1',
+    left: '0'
+  }, 1000);
+
+  $(".instruktur-2").delay(1000)
+  .animate({
+    opacity: '1',
+    right: '0'
+  }, 1000);
+
+  $(".instruktur-3").delay(1500)
+  .animate({
+    opacity: '1',
+    left: '0'
+  }, 1000);
+
+});
